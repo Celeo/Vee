41abde6957c09872adf7e26a9a5892d26a2d1454
Celeo <celeodor@gmail.com>
2015-03-20 16:30:08 -0700

    Work on voting on submissions on their comments page

--- vee.js
+++ vee.js
@@ -113,19 +113,27 @@ $(document).keypress(function(e) {
         case 101:
             // e - vote up
             getDivs(true).each(function() {
-                    var arrow = $(this).parent().find('div.unvoted').find('div.arrow-upvote');
-                    if (arrow.length > 0)
-                        arrow.first().click();
-                    return false;
+                var arrow = $(this).parent().find('div.unvoted').find('div[class^="arrow-upvote"]');
+                if (arrow.length > 0)
+                    arrow.click();
+                else {
+                    arrow = $(this).parent().find('div.midcol').find('div[class^="arrow-upvote"]');
+                    arrow.first().click();
+                }
+                return false;
             });
             break;
         case 100:
             // d - vote down
             getDivs(true).each(function() {
-                    var arrow = $(this).parent().find('div.unvoted').find('div.arrow-downvote');
-                    if (arrow.length > 0)
-                        arrow.click();
-                    return false;
+                var arrow = $(this).parent().find('div.unvoted').find('div[class^="arrow-downvote"]');
+                if (arrow.length > 0)
+                    arrow.click();
+                else {
+                    arrow = $(this).parent().find('div.midcol').find('div[class^="arrow-downvote"]');
+                    arrow.first().click();
+                }
+                return false;
             });
             break;
     }
